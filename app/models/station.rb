7fb3491ae6b12e2d9b25676eebc66b9652cecfd6
abarnes26 <abarnes26@yahoo.com>
2018-02-07 10:13:26 -0700

    building station and sorting out api call issues

---
+++ app/models/station.rb
@@ -0,0 +1,18 @@
+class Station
+
+  def initialize(station_json)
+    binding.pry
+    @name = station_list
+    @address = station_list
+    @fuel_types = station_list
+    @distance = station_list
+    @access_times = station_list
+  end
+
+  # t.string "name"
+  # t.string "address"
+  # t.string "fuel_types"
+  # t.float "distance"
+  # t.datetime "access_times"
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180207165904) do
+
+  create_table "stations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "fuel_types"
+    t.float "distance"
+    t.datetime "access_times"
+  end
 
 end
